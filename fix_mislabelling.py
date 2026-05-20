fe175bf793ec8fbb1f6d60ba961813f832a38d1f
Sonia <soniasohail566@gmail.com>
2026-05-20 13:14:56 -0400

    working through mislabelling code

--- fix_mislabelling.py
+++ fix_mislabelling.py
@@ -5,24 +5,33 @@ import pandas as pd
 import scipy.io as sio
 import matplotlib.pyplot as plt
 from signal_postprocessing import replace_missing
-from missing_gaps_stats import import_data, find_missing_segments_indices
+from itertools import chain
+from missing_gaps_stats import import_data
+from signal_postprocessing import lin_interp_threshold
 
 keypoints_folder = '/mnt/c/3HYPER FREEPLAY DV METRABS/MATLAB Keypoints 2/2D Keypoints'
 dyad_mislabelling_list = '3HYPER Joint Keypoint Label Swapping Log.csv'
 dst_dir = '/mnt/c/3HYPER FREEPLAY DV METRABS/MATLAB Keypoints 2/2D Keypoints Swapped'
-selected_joint_indices = [15, 16, 17]
+selected_joint_indices = [16]
 
-def filter_nans_from_indices(infant_data, parent_data, selected_indices):
-    filtered_infant_data, infant_nan = replace_missing(infant_data)
-    filtered_parent_data, parent_nan = replace_missing(parent_data)
+def filter_nans_from_indices(infant_signal, parent_signal, selected_indices):
+    infant_signal, _ = replace_missing(infant_signal)
+    parent_signal, _ = replace_missing(parent_signal)
+
+    all_selected_indices = np.array(list(chain.from_iterable(selected_indices)), dtype=int)
+
+    valid_mask = (
+        ~np.isnan(infant_signal[all_selected_indices]) &
+        ~np.isnan(parent_signal[all_selected_indices])
+    )
     
-    for indices in selected_indices:
-        start = indices[0]
-        end = indices[-1]
-        if np.isnan(filtered_infant_data[:, :, start:end]).any() or np.isnan(filtered_parent_data[:, :, start:end]).any():
-            selected_indices.remove(indices)
-            
-    return selected_indices
+    filtered_indices = all_selected_indices[valid_mask].tolist()
+    
+    nan_indices = np.where(~np.isnan(infant_signal[all_selected_indices]) & ~np.isnan(parent_signal[all_selected_indices]))
+    
+    for index in nan_indices:
+        nearest_point = filtered_indices
+    return filtered_indices
 
 def import_select_participants(list_of_unique_dyads, folder_path):
     dyad_info_collection = {}
@@ -32,7 +41,7 @@ def import_select_participants(list_of_unique_dyads, folder_path):
         if dyad >= 100:
             file_name = "3HYPER." + str(dyad) + " FREEPLAY DV EXTRACTED 2D Keypoints.mat"
         else:
-            file_name = "3HYPER.0" +str(dyad) + " FREEPLAY DV EXTRACTED 2D Keypoints.mat"
+            file_name = "3HYPER.0" + str(dyad) + " FREEPLAY DV EXTRACTED 2D Keypoints.mat"
             
         file_path = os.path.join(folder_path, file_name)
         
@@ -48,11 +57,9 @@ def swap_keypoints(infant_data, parent_data, list_of_indices):
     original_parent = copy.deepcopy(parent_data)
     swapped_parent = copy.deepcopy(parent_data)
     
-    for indices in list_of_indices:
-        start = indices[0]
-        end = indices[-1]
-        swapped_infant[start:end] = original_parent[start:end]
-        swapped_parent[start:end] = original_infant[start:end]
+    list_of_indices = list(chain.from_iterable(list_of_indices))
+    swapped_infant[list_of_indices] = original_parent[list_of_indices]
+    swapped_parent[list_of_indices] = original_infant[list_of_indices]
         
     return swapped_infant, swapped_parent
 
@@ -81,10 +88,28 @@ def save_keypoints_in_mat(dyad_number, swapped_infant_data, swapped_parent_data,
     dyad_keypoints = {"Infant": swapped_infant_data, "Parent": swapped_parent_data}
     print(f"Saving to .... {full_file_path}")
     sio.savemat(full_file_path, dyad_keypoints)
+    
+def plot_interval(original_data, swapped_data, joint, coordinate, start=0, end=7000, title="Original vs Swapped"):
+    original_signal = original_data[start:end]
+    swapped_signal = swapped_data[start:end]
+
+    t = np.arange(start, end)
+
+    plt.figure(figsize=(12, 5))
+    plt.plot(t, original_signal, label="Original", alpha=0.7)
+    plt.plot(t, swapped_signal + 10, label="Swapped", alpha=0.7)
+
+    plt.title(f"{title} | Joint {joint}, Coord {coordinate} | Frames {start}-{end}")
+    plt.xlabel("Frame")
+    plt.ylabel("Value")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
         
 def main():
     
-    selected_dyads = [40, 59, 108]
+    selected_dyads = [40]
     selected_dyad_keypoints = import_select_participants(selected_dyads, keypoints_folder)
     swap_log = pd.read_csv(dyad_mislabelling_list)
     
@@ -94,24 +119,36 @@ def main():
         
         print(f"Swapping keypoints for Dyad #{dyad}")
         all_swap_indices = find_all_swap_indices(swap_log, dyad)
-        filtered_swapped_indices = filter_nans_from_indices(original_infant_selected_dyad_keypoints, original_parent_selected_dyad_keypoints, all_swap_indices)
+        print(f"Swapping at Indices: {all_swap_indices}")
         
         for joint in selected_joint_indices:
             for coordinate in range(2):
                 # Get original keypoint data
-                infant_signal = selected_dyad_keypoints[dyad]["infant"][joint, coordinate, :]
-                parent_signal = selected_dyad_keypoints[dyad]["parent"][joint, coordinate, :]
+                infant_signal, _ = replace_missing(lin_interp_threshold(selected_dyad_keypoints[dyad]["infant"][joint, coordinate, :], 12))
+                parent_signal, _ = replace_missing(lin_interp_threshold(selected_dyad_keypoints[dyad]["parent"][joint, coordinate, :], 12))
                 
                 # Swap keypoints at necessary indices
-                swapped_infant_signal, swapped_parent_signal  = swap_keypoints(infant_signal, parent_signal, filtered_swapped_indices)
+                swapped_infant_signal, swapped_parent_signal = swap_keypoints(infant_signal, parent_signal, all_swap_indices)
+                swapped_infant_signal, _ = replace_missing(swapped_infant_signal)
+                swapped_parent_signal, _ = replace_missing(swapped_parent_signal)
                 print("Swapping complete.")
+                
+                start=0
+                stop=len(swapped_infant_signal)
+                
+                print(f"Original infant: {infant_signal[5190:5202]}")
+                print(f"Swapped infant: {swapped_infant_signal[5190:5202]}")
+                print(f"Original parent: {parent_signal[5190:5202]}")
+                print(f"Swapped parent: {swapped_parent_signal[5190:5202]}")
                 
                 selected_dyad_keypoints[dyad]["infant"][joint, coordinate, :] = swapped_infant_signal
                 selected_dyad_keypoints[dyad]["parent"][joint, coordinate, :] = swapped_parent_signal
                 
+                # plot_interval(infant_signal, swapped_infant_signal, joint, coordinate, start, stop)
+                
         # Save keypoints to new .mat file
-        print(f"Saving swapped keypoints for Dyad #{dyad}")
-        save_keypoints_in_mat(dyad, selected_dyad_keypoints[dyad]["infant"], selected_dyad_keypoints[dyad]["parent"], dst_dir)
+        # print(f"Saving swapped keypoints for Dyad #{dyad}")
+        # save_keypoints_in_mat(dyad, selected_dyad_keypoints[dyad]["infant"], selected_dyad_keypoints[dyad]["parent"], dst_dir)
         
 if __name__ == "__main__":
     main()
--- full_preprocessing.py
+++ full_preprocessing.py
@@ -2,43 +2,128 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as sio
-from missing_gaps_stats import import_data
-from signal_postprocessing import lin_interp_threshold, butterworth_filter, find_missing_segments_indices, movmad_filter
+from missing_gaps_stats import import_data, get_dyad_number
+from signal_postprocessing import replace_missing, lin_interp_threshold, butterworth_filter, find_missing_segments_indices, movmad_filter
 
 # Cleaning and interpolating signals for all dyads except excluded ones
 GAP_THRESHOLD_FRAMES = 12
+JOINT_NAMES = ["Neck", "Head", "Left Shoulder", "Right Shoulder"]
 keypoints_dir = "/mnt/c/3HYPER FREEPLAY DV METRABS/MATLAB Keypoints 2/2D Keypoints"
-excluded_dyads = [57, 76, 78, 112]
-desired_joint_indices = [15, 16, 17]
+excluded_dyads = [57, 76, 78, 112, 40, 108, 31]
+desired_joint_indices = [12, 15, 16, 17, 18, 19]
+
+def save_keypoints_in_mat(dyad_number, processed_infant_data, processed_parent_data, destination_folder_path):
+    # Prepare file path for saving
+    if dyad_number >= 100:
+        file_name = "3HYPER." + str(dyad_number) + " FREEPLAY DV EXTRACTED 2D Swapped Keypoints.mat"
+    else:
+        file_name = "3HYPER.0" +str(dyad_number) + " FREEPLAY DV EXTRACTED 2D Swapped Keypoints.mat"
+            
+    full_file_path = os.path.join(destination_folder_path, file_name)
+        
+    # Create dictionary object to place infant and parent keypoints 
+    dyad_keypoints = {"Infant": processed_infant_data, "Parent": processed_parent_data}
+    print(f"Saving to .... {full_file_path}")
+    sio.savemat(full_file_path, dyad_keypoints)
 
 def normalize_signal(data_x, data_y):
-    normalized_signal = np.
+    normalized_signal = np.sqrt(np.pow(data_x, 2) + np.pow(data_y, 2))
+    
+    return normalized_signal
+
+def plot_original_vs_preprocessed_signals(infant_original_data, parent_original_data, infant_normalized_data, parent_normalized_data, dyad_number, joint_index):
+    fig, ax = plt.subplots(2, 2, sharex=True)
+    
+    frames_x = np.arange(len(parent_original_data))
+    
+    fig.suptitle(f"Original vs. Preprocessed for Dyad #{dyad_number}")
+
+    ax[0,0].plot(frames_x, infant_original_data)
+    ax[0,0].set_title(f"Infant (Original)")
+    
+    ax[1,0].plot(frames_x, infant_normalized_data)
+    ax[1,0].set_title(f"Infant (Preprocessed)")
+    
+    ax[0,1].plot(frames_x, parent_original_data)
+    ax[0,1].set_title(f"Parent (Original)")
+    
+    ax[1,1].plot(frames_x, parent_normalized_data)
+    ax[1,1].set_title(f"Parent (Preprocessed)")
+    
+    plt.show()
 
 def main():
 
     for file in os.listdir(keypoints_dir):
         keypoint_path = os.path.join(keypoints_dir, file)
+        dyad_number = get_dyad_number(file)
+        
+        if dyad_number in excluded_dyads:
+            print(f"Skipping Dyad #{dyad_number} due to exclusion criteria.")
+            continue
+        
+        print(f"Loading Dyad #{dyad_number} .....")
         dyad_keypoints = import_data(keypoint_path)
+        infant_keypoints = np.array(dyad_keypoints["infant"])
+        parent_keypoints = np.array(dyad_keypoints["parent"])
         
-        infant_keypoints = np.array(dyad_keypoints["Infant"])
-        parent_keypoints = np.array(dyad_keypoints["Parent"])
+        infant_max_frames = infant_keypoints.shape[2]
+        parent_max_frames = parent_keypoints.shape[2]
         
-        for joint in desired_joint_indices:
+        infant_intermediate_keypoints = np.ones_like(infant_keypoints)
+        parent_intermediate_keypoints = np.ones_like(parent_keypoints)
+        
+        infant_modified_keypoints = np.zeros((len(desired_joint_indices), infant_max_frames))
+        parent_modified_keypoints = np.zeros((len(desired_joint_indices), parent_max_frames))
+        
+        for (joint, new_joint) in (zip(desired_joint_indices, range(len(desired_joint_indices)))):
             for coordinate in range(2):
                 infant_original_signal = infant_keypoints[joint, coordinate, :]
                 parent_original_signal = parent_keypoints[joint, coordinate, :]
+                
+                # Replace 0s with NaN
+                infant_original_signal, _ = replace_missing(infant_original_signal)
+                parent_original_signal, _ = replace_missing(parent_original_signal)
 
                 # Use thresholded linear interpolation on missing data
+                print(f"Interpolating keypoints .....")
                 infant_interpolated_signal = lin_interp_threshold(infant_original_signal, GAP_THRESHOLD_FRAMES)
                 parent_interpolated_signal = lin_interp_threshold(parent_original_signal, GAP_THRESHOLD_FRAMES)
                 
                 # Median filtering 
-                infant_filtered_signal = movmad_filter(infant_interpolated_signal, 30)
-                parent_filtered_signal = movmad_filter(parent_interpolated_signal, 30)
+                print("Filtering keypoints with median filtering .....")
+                infant_intermediate_keypoints[new_joint, coordinate, :] = movmad_filter(infant_interpolated_signal, 30)
+                parent_intermediate_keypoints[new_joint, coordinate, :] = movmad_filter(parent_interpolated_signal, 30)
                 
-        # Normalize signal 
-        infant_normalized_signal = np.sqrt((infant_keypoints[:, 0, :]**2) + (infant_keypoints[:, 1, :]**2))
-        parent_normalized_signal = np.sqrt((parent_keypoints[:, 0, :]**2) + (parent_keypoints[:, 1, :]**2))
+            # Normalize signal 
+            print("Normalize signal using L2 norm .....")
+            infant_x, infant_y = infant_intermediate_keypoints[new_joint, 0, :], infant_intermediate_keypoints[new_joint, 1, :]
+            parent_x, parent_y = parent_intermediate_keypoints[new_joint, 0, :], parent_intermediate_keypoints[new_joint, 1, :]
+            
+            infant_normalized_signal = normalize_signal(infant_x, infant_y)
+            parent_normalized_signal = normalize_signal(parent_x, parent_y)
+            
+            # Add nornalized keypoints to new numpy array for saving
+            print(f"Saving modified signals to new array ......")
+            infant_modified_keypoints[new_joint, :] = infant_normalized_signal
+            parent_modified_keypoints[new_joint, :] = parent_normalized_signal
+            
+            # Plot keypoints for verification
+            plot_original_vs_preprocessed_signals(infant_keypoints[joint, coordinate, :], parent_keypoints[joint, coordinate, :], infant_modified_keypoints[new_joint, :], 
+                                                  parent_modified_keypoints[new_joint, :], dyad_number)
+            
+            # Save keypoints to .mat file for analysis 
+            
+            
+            
+if __name__ == "__main__":
+    main()
+
+            
+            
+            
+            
+            
         
         
             
