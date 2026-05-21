72fb5779e2619f365d86b5453f9a3cb94e46d1b0
soniasohail5 <soniasohail566@gmail.com>
2026-05-20 23:44:43 -0400

    remove unused files

--- import_data.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-print("Syncing dataset...")
-subprocess.run(["bash", "sync_data.sh"], check=True)
-
-print("Dataset import is complete.")
--- sync_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-REMOTE="infantresearch@2MC9W54:/C:/3HYPER FREEPLAY DV METRABS/MATLAB Keypoints 2/2D Keypoints"
-LOCAL="../data"
-
-echo "Syncing dataset..."
-rsync -avz $REMOTE $LOCAL -vvv
-
-echo "Done."
